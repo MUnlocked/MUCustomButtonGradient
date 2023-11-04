b8bc9a7eb317265b83598b1567cfca39193a42a4
MUnlocked <MUnlocked1000@outlook.com>
2023-11-04 04:16:00 -0500

    first commit

---
+++ CustomBtnGD/Custom/BtnGDIMG.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace MUnlockedCustomButttonGradient
+{
+    public partial class BtnGDIMG : UserControl
+    {
+        private int wh = 20;
+        private float ang = 45;
+        private Color cl0 = Color.DarkTurquoise;
+        private Color cl1 = Color.Magenta;
+        private Timer t = new Timer();
+        private string buttonText = "Ingresa tu texto";
+        private Font buttonTextFont = DefaultFont;
+        private Point buttonImageLocation = new Point(0, 0);
+
+
+        public BtnGDIMG()
+        {
+            DoubleBuffered = true;
+            t.Interval = 1;
+            t.Start();
+            t.Tick += (s, e) => { Angle = Angle % 360 + 1; };
+            ForeColor = Color.White;
+            ImageWidth = 150;
+            ImageHeight = 150;
+        }
+
+        public int TextX { get; set; }
+        public int TextY { get; set; }
+
+        public Image ButtonImage { get; set; }
+
+        public int ImageWidth { get; set; }
+        public int ImageHeight { get; set; }
+
+        public int ImageX
+        {
+            get { return buttonImageLocation.X; }
+            set { buttonImageLocation.X = value; Invalidate(); }
+        }
+
+        public int ImageY
+        {
+            get { return buttonImageLocation.Y; }
+            set { buttonImageLocation.Y = value; Invalidate(); }
+        }
+
+        public int BorderRadius
+        {
+            get { return wh; }
+            set { wh = value; Invalidate(); }
+        }
+
+        public float Angle
+        {
+            get { return ang; }
+            set { ang = value; Invalidate(); }
+        }
+
+        public Color Color0
+        {
+            get { return cl0; }
+            set { cl0 = value; Invalidate(); }
+        }
+
+        public Color Color1
+        {
+            get { return cl1; }
+            set { cl1 = value; Invalidate(); }
+        }
+
+        public string ButtonText
+        {
+            get { return buttonText; }
+            set { buttonText = value; Invalidate(); }
+        }
+
+        public Font ButtonTextFont
+        {
+            get { return buttonTextFont; }
+            set { buttonTextFont = value; Invalidate(); }
+        }
+
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddArc(new Rectangle(0, 0, wh, wh), 180, 90);
+            gp.AddArc(new Rectangle(Width - wh, 0, wh, wh), -90, 90);
+            gp.AddArc(new Rectangle(Width - wh, Height - wh, wh, wh), 0, 90);
+            gp.AddArc(new Rectangle(0, Height - wh, wh, wh), 90, 90);
+
+            e.Graphics.FillPath(new LinearGradientBrush(ClientRectangle, cl0, cl1, ang), gp);
+
+            if (ButtonText != null)
+            {
+                SizeF textSize = e.Graphics.MeasureString(buttonText, Font);
+                float textX = (Width - textSize.Width) / 2 + TextX;
+                float textY = Height - textSize.Height - 10 + TextY;
+                e.Graphics.DrawString(buttonText, Font, new SolidBrush(ForeColor), textX, textY);
+            }
+
+
+            if (ButtonImage != null)
+            {
+                int imageWidth = ButtonImage.Width;
+                int imageHeight = ButtonImage.Height;
+                int imageX = buttonImageLocation.X;
+                int imageY = buttonImageLocation.Y;
+
+                e.Graphics.DrawImage(ButtonImage, imageX, imageY, ImageWidth, ImageHeight);
+            }
+        }
+    }
+}
+    
---
+++ CustomBtnGD/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("MU Custom Button Gradient")]
+[assembly: AssemblyDescription("Custom Button Gradient Animated")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MUnlocked Dev")]
+[assembly: AssemblyProduct("MUCustomButtonGradient")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e8aef6bc-1d3d-4ba6-9e08-c6431245832b")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
